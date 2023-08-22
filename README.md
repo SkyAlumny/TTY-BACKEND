9df00bb37c2d71e4793d95885b65bc071a46cdfb
SkyAlumny <dizifake@gmail.com>
2023-08-22 18:50:58 +0200

    Regiser API

--- README.md
+++ README.md
@@ -3,27 +3,10 @@
 
 ## 📂 New Features
 
-[+] Account check API
-[+] Status Bot
+- [+] Account check API
+- [+] Status Bot
+- [+] Account register via API
 
 ## 📄 DOCS
 
-#### Account Check api usage
-
-- Located: routes/api.js | Line 6
-- URL: IP:PORT/api/accountcheck
-- Parameters: IP:PORT/apo/accountcheck?email=EMAIL&password=PASSWORD
-
-| CODE | STATUS                                           |
-|------|--------------------------------------------------|
-| 200  | Valid Login                                      |
-| 401  | Password invalid (E-Mail is on the Database)     |
-| 404  | E-Mail not found (E-Mail is not on the Database) |
-
-
-#### Status Bot
-Make sure to insert the Channel ID at config.json where you want the status message
-The Status Bot is build to send the embed to a channel that is completly empty and the only messages should be the message from the Bot
-
-
-more soon
+Check the [WIKI Page](https://github.com/SkyAlumny/LawinServerV2-Modified/wiki)
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../model/user.js");
+const functions = require("../structs/functions.js")
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("./Config/config.json").toString());
+
+
+app.get("/api", async (req, res) =>{
+    const currentDate = new Date();
+   res.json({ currentDate: currentDate.toISOString(), status: "online" });
+   return res.status(200)
+})
 
 app.get("/api/accountcheck", async (req, res) => {
     const { email, password } = req.query;
@@ -34,5 +44,58 @@ app.get("/api/accountcheck", async (req, res) => {
   
 });
 
+app.post("/api/register", async (req, res) => {
+    const { apikey, discordid, username, email, password } = req.query;
+    if (!apikey)
+    {
+        res.json({ code: "400", error: "APIKEY no provided" });
+        return res.status(400)
+    }
+    if (!discordid)
+    {
+        res.json({ code: "400", error: "Discord ID not provided" });
+        return res.status(400)
+    }
+    if (!username)
+    {
+        res.json({ code: "400", error: "Username not provided" });
+        return res.status(400)
+    }
+    if (!email)
+    {
+        res.json({ code: "400", error: "E-Mail not provided" });
+        return res.status(400)
+    }
+    if (!password)
+    {
+        res.json({ code: "400", error: "Pasword not provided" });
+        return res.status(400)
+    }
+    
+   
+    if (apikey == config.APIKEY) {
+        try {
+            await functions.registerUser(discordid, username, email, password).then(response => {
+              if (!response.status == 400)
+              {
+                res.json({ code: "200", error: "SUCCESS" });
+                return res.status(200)
+              }
+
+              res.json({ code: response.status, error: response.message });
+              return res.status(response.status)
+            })
+         
+        }
+        catch (err) {
+            res.json({ code: "500", error: err });
+            return res.status(500)
+        }
+    }
+    else {
+        res.json({ code: "401", error: "Invalid API key" });
+        return res.status(401)
+    }
+});
 
 module.exports = app;
